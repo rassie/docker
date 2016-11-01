569a34e2ade047b078d1a910e0b09291fe78e72f
Nikolai Prokoschenko <nikolai@prokoschenko.de>
2016-11-02 00:27:54 +0100

    bla

--- 1.12/experimental/docker-entrypoint.sh
+++ 1.12/experimental/docker-entrypoint.sh
@@ -14,7 +14,7 @@ fi
 
 # if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
 if [ -z "$DOCKER_HOST" -a "$RASSIE_DOCKER_PORT_2375_TCP" ]; then
-	export DOCKER_HOST='tcp://rassie_docker:2375'
+	export DOCKER_HOST='tcp://rassie__docker:2375'
 fi
 
 exec "$@"
