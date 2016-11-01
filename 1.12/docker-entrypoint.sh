975c90190ec543d7d3b6af93d4d1b6b4fc59b8af
Nikolai Prokoschenko <nikolai@prokoschenko.de>
2016-11-01 23:44:24 +0100

    Set the environment variables correctly
    (TODO: dismiss their usage)

--- 1.12/docker-entrypoint.sh
+++ 1.12/docker-entrypoint.sh
@@ -13,7 +13,7 @@ if docker "$1" --help > /dev/null 2>&1; then
 fi
 
 # if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
-if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
+if [ -z "$DOCKER_HOST" -a "$RASSIE_DOCKER_PORT_2375_TCP" ]; then
 	export DOCKER_HOST='tcp://docker:2375'
 fi
 
